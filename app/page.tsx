c296e721a60bc9ae84c689eea7b5654d7994e7c6
Ricnts03 <148211226+ricardo-santos-a22207422@users.noreply.github.com>
2024-12-12 09:51:02 +0000

    errors in home text fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,6 @@ import React from 'react'
 export default function page() {
   return <>
     <h1>React e Next.js</h1>
-    <p>Bem-vindo à minha app em React e Next.js, das tcnologias Web mai usadas nos dias de hoje.</p>
+    <p>Bem-vindo à minha app em React e Next.js, das tecnologias Web mais usadas nos dias de hoje.</p>
   </>
 }
